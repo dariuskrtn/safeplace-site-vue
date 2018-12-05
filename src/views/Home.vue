ed7b4d09e04107a61d37f18b9d252eebdbe2e0f6
Tomas Mikna <TMikna@users.noreply.github.com>
2018-12-05 19:34:28 +0200

    Home and Register views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,144 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="home">
+        <div class="window">
+            <div class="floor">
+              
+                floor
+
+                <div>
+                    <h4> {{currentFloorName}} </h4>
+                </div>
+                <div>
+                   <ul id="v-for-object" class="demo">
+                        <li v-for="value in getCamera('1c5167cd-95c3-4ad9-9d6d-5f3067b85566')">
+                            {{ value }}
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                   <ul class="demo">
+                        <li v-for="value in getCurrentCameras()">
+                            {{ value }}
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <img <!--src="http://localhost:54507/api/images/2d0c3a05-5b96-49d5-b20e-4b3a9de89357" --> alt = "Failed to upload floor image">
+                </div>
+
+
+            </div>
+            <div class="navigation">
+                navigation
+                <div class="navigation-buttons">
+                    <button v-on:click="ShowUpperFloor()"> Previous floor </button>
+                    <button v-on:click="ShowLowerFloor()"> Next floor </button>
+                </div>
+                <hr/>
+                <div>
+                    <li v-for="floor in floorList" :key="floor.guid">
+                       {{ floor.name }}
+                    </li>
+                </div>
+                <hr/>
+                <div>
+                    Hovered camera spotted people:
+                  <ul style="list-style: none;">
+                    <li><pre>Name, Surname:</pre></li>
+                    <li v-for="person in spottedPeople" :key="person.guid">
+                      
+                       {{ person.name }} {{person.lastName}}
+                       
+                    </li>
+                  </ul>
+                </div>
+            </div>
+              
+
+        </div>
+    </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+// import { Component, Vue } from 'vue-property-decorator';
+// import store from '@/store/store';
+// import Camera from '@/models/Camera';
+// import Floor from '@/models/Floor';
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+    
+    data () {
+        return {
+            floorList: this.$store.state.floor.floorList;
+            cameraList: this.$store.state.camera.cameraList;
+            currentFloor: this.$store.state.floor.currentFloor;
+            //when putting currentFloor.name it destroys page when currentFloor is null
+            currentFloorName:  this.currentFloor.name;
+            JustCameras : this.currentFloor.cameras;
+        }
+    },
+
+    watch: {
+
+    },
+
+    methods: {
+       ShowUpperFloor: function(){
+        var index = this.$store.state.floor.floorList.indexOf(this.currentFloor);
+        if (index > 0)
+        this.currentFloor = this.$store.state.floor.currentFloor = this.$store.state.floor.floorList[index-1]; 
+      },
+   
+      ShowLowerFloor(){
+        var index = this.$store.state.floor.floorList.indexOf(this.currentFloor);
+        if (index < this.$store.state.floor.floorList.length-1)
+        this.currentFloor = this.$store.state.floor.currentFloor = this.$store.state.floor.floorList[index+1]; 
+      },
+
+          // does not work as expected
+      getCurrentCameras(){
+        var someCameras = [];
+            this.currentFloor.cameras.forEach(camera => {
+              var temp = this.cameraList.find(cam => cam.guid == "95777b1b-9f98-4bad-8fff-837a7b41dc63");
+              //[Tomas] a bit of workaround, can not find better solution - find can return undefine, and camera can not be ondefined (can;t null as well)
+              camera = temp? temp : camera;
+              someCameras.push(camera);
+            })
+            return someCameras;
+        },
+
+      getCamera(guid : string){
+        return this.cameraList.find(cam => cam.guid == guid);
+      } 
+    }
+
+    
 }
 </script>
+
+
+
+
+<style>
+    .navigation-buttons button {
+    display: block;
+    width: 60%;
+    margin-left: auto;
+    margin-right: auto;
+
+    }
+    .window {
+        display: flex;
+        flex: 1 1 0%;
+        flex-direction: row-reverse;
+    }
+    .floor {
+        flex: 1 1 0%;
+    }
+    .navigation{
+        flex: 0 2 30%;
+        max-width: 420px;
+        min-width: 200px;
+    }
+
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,151 @@
+<template>
+    <div id="register">
+        <div class="row">
+            <div class="col-md-4">
+                <video ref="video" id="video" width="640" height="480" autoplay></video>
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-4">Name:</div>
+                    <div class="col-md-6">
+                        <input type="text" class="form-control">
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-md-4">Last Name:</div>
+                    <div class="col-md-6">
+                        <input type="text" class="form-control">
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-md-4">Allowed Cameras:</div>
+                    <div class="col-md-6">
+                        <select class="form-control" multiple style="height: 150px">
+                            <option v-for="cam in cameras" v-bind:value="cam.value">
+                                {{ cam.name }}
+                            </option>
+                        </select>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-md-12">
+                        <h6>Pictures:</h6>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <button class="btn btn-default" id="snap" v-on:click="toggleCamera()">{{ isCameraPlaying ? "Turn camera off" : "Turn camera on" }}</button>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <button :disabled="captures.length >= picturesRequired" class="btn btn-default" id="snap" v-on:click="capture()">Snap Photo</button>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <button :disabled="captures.length < picturesRequired" class="btn btn-success" id="snap" v-on:click="savePerson()">Save Person</button>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-12">
+                                Pictures: {{ captures.length }} / {{ picturesRequired }}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div v-for="c in captures" class="col-md-3">
+                                <img v-bind:src="c" height=50 />
+                                <br><br>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <canvas ref="canvas" id="canvas" width="640" height="480"></canvas>
+    </div>
+</template>
+
+<script lang="js">
+// import Vue from 'vue';
+// import Component from 'vue-class-component';
+// import Camera from '@/models/Camera';
+
+@Component({})
+export default {
+
+    data (){
+        return {
+            name: "",
+            lastName: "",
+            cameras: $store.state.camera.cameraList,
+            isCameraPlaying: false,
+            picturesRequired: 10,
+
+            video: null,
+            stream: null,
+            canvas: null,
+            captures: []
+        }
+    }
+
+    mounted() {
+        this.video = this.$refs.video;
+    }
+
+    methods: {
+        toggleCamera() {
+            if (this.isCameraPlaying) this.stopCamera();
+            else this.startCamera();
+            this.isCameraPlaying = !this.isCameraPlaying;
+        }
+        startCamera() {
+            if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+                navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+                    this.video.src = window.URL.createObjectURL(stream);
+                    this.video.play();
+                    this.stream = stream;
+                });
+            }
+        }
+        stopCamera() {
+            if (this.stream != null && this.stream.active) {
+                this.stream.getVideoTracks()[0].stop();
+            }
+        }
+
+        capture() {
+            this.canvas = this.$refs.canvas;
+            var context = this.canvas.getContext("2d").drawImage(this.video, 0, 0, 640, 480);
+            this.captures.push(this.canvas.toDataURL("image/png"));
+        }
+
+        savePerson() {
+            //TODO: this
+        }
+    }
+
+}
+</script>
+
+
+<style>
+    #video {
+        background-color: #000000;
+    }
+    #canvas {
+        display: none;
+    }
+</style>
